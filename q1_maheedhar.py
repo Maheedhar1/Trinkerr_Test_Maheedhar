4c21a4225a3bb45e97f2542dca8410b7b05f6b70
Maheedhar1 <maheedhar.bellum@robo4me.com>
2022-04-30 05:33:43 +0530

    Update q1_maheedhar.py

--- q1_maheedhar.py
+++ q1_maheedhar.py
@@ -2,23 +2,21 @@ def optimalways(colour_arr):
  
 
     c = 0
- 
-    colour_arr.sort()
-    i = 0
-    while i < (len(colour_arr)-1):
-
-        if (colour_arr[i] == colour_arr[i + 1]):
-            c += 1
- 
-    
-            i = i + 2
-
+    colour_count_dict ={}
+    for i in colour_arr:
+        if i in colour_count_dict.keys():
+            colour_count_dict[i] = colour_count_dict[i] + 1
         else:
-            i += 1
- 
-    return c
- 
-
+            colour_count_dict[i] = 1
+    total_optimal_ways = 0
+    for i in colour_count_dict.keys():
+        k = colour_count_dict[i]
+        if k >1:
+            
+            ways = k * (k-1)/2
+            total_optimal_ways = total_optimal_ways + ways
+    return int(total_optimal_ways)
+    
 if __name__ == "__main__":
  
     colour_list = [1,2,3,1,2]
